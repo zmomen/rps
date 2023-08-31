fec7e8db8e126f7fd8647b9372551332f8e5547e
zmomen <zalmomen01@gmail.com>
2023-08-31 16:16:23 -0500

    Add basic handlers for POST and GET requests

--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
-    "net/http"
-    
-    "github.com/labstack/echo/v4"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+func processReceipt(c echo.Context) error {
+	return c.JSON(http.StatusCreated, 201)
+}
+
+func getReceiptPoints(c echo.Context) error {
+
+	return c.JSON(http.StatusOK, "points response")
+}
+
+func homePage(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello! from RPS - Receipt Processor Service!")
+}
+
 func main() {
-    e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
-    e.Logger.Fatal(e.Start(":1323"))
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", homePage)
+	e.POST("/receipts/process", processReceipt)
+	e.GET("/receipts/:id/points", getReceiptPoints)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
